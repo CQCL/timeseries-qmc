1412f49c4a18df8702e1a907bcd6575969a9b4eb
Khaldoon Ghanem <khaldoon.ghanem@quantinuum.com>
2023-08-10 11:29:34 +0200

    update setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+project_urls = {
+  'Documentation': 'https://cqcl.github.io/timeseries-qmc/build/html/index.html',
+  'Source': 'https://github.com/CQCL/timeseries-qmc'
+}
+
 setup(
     name='timeseries_qmc',
     version='0.1.0',    
     description='A Python package for running the time series QMC algorithm',
-    url='https://https://github.com/CQCL/timeseries-qmc',
+    project_urls = project_urls,
     author='Quantinuum GmbH',
     author_email='khaldoon.ghanem@quantinuum.com',
     license='Apache License 2.0',
@@ -25,4 +34,6 @@ setup(
         'Operating System :: MacOS :: MacOS X',       
         'Programming Language :: Python :: 3',
     ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
